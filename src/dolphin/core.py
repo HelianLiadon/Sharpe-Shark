ae84acdb5a0184df45fac32cb05f960adbc4ff6e
Helian <adjeenli@gmail.com>
2017-11-24 21:19:30 +0100

    Add asset-fetching functions to API wrapper

--- src/dolphin/core.py
+++ src/dolphin/core.py
@@ -12,3 +12,23 @@ def SetServerInfo(url, creds):
 
 def GetServerInfo():
     return (BASEURL, AUTH)
+
+def GetAllAssets():
+    try:
+        r = requests.get(BASEURL + '/asset', auth=AUTH, verify=False)
+        return json.loads(r.text)
+
+    except BaseException as Err:
+        print("[GetAllAssets] Unhandled exception: " + str(Err))
+        raise
+
+def GetAsset(AssetId):
+    try:
+        r = requests.get(BASEURL + '/asset/' + str(AssetId),
+                         auth=AUTH,
+                         verify=False)
+        return json.loads(r.text)
+
+    except BaseException as Err:
+        print("[GetAsset] Unhandled exception: " + str(Err))
+        raise
