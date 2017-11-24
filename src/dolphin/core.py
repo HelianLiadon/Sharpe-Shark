1a2a23b3dff8f3777cd6016b913d62aaff4c7c15
Helian <adjeenli@gmail.com>
2017-11-24 22:20:07 +0100

    Add ratio-related wrapper functions

--- src/dolphin/core.py
+++ src/dolphin/core.py
@@ -4,6 +4,17 @@ import json
 AUTH = ()
 BASEURL = ""
 
+class Ratios:
+    BETA = 15
+    PEARSON = 19
+    RENDEMENT = 21
+    RENDEMENT_ANN = 17
+    SHARPE = 20
+    VAR = 22
+    VOLATILITE = 18
+    EXPOSITION = 29
+
+
 def SetServerInfo(url, creds):
     global AUTH
     global BASEURL
@@ -31,4 +42,30 @@ def GetAsset(AssetId):
 
     except BaseException as Err:
         print("[GetAsset] Unhandled exception: " + str(Err))
+        raise
+
+def GetRatios():
+    try:
+        r = requests.get(BASEURL + '/ratio',
+                         auth=AUTH,
+                         verify=False)
+        return json.loads(r.text)
+
+    except BaseException as Err:
+        print("[GetRatios] Unhandled exception: " + str(Err))
+        raise
+
+def ComputeRatios(PortfolioId, Rats=[Ratios.SHARPE]):
+    try:
+        data = {"asset": PortfolioId, "ratio": Rats}
+        jdata = json.dumps(data)
+        r = requests.post(BASEURL + "/ratio/invoke",
+                         data=jdata,
+                         auth=AUTH,
+                         verify=False)
+
+        return json.loads(r.text)
+
+    except BaseException as Err:
+        print("[ComputeRatios] Unhandled exception: " + str(Err))
         raise
